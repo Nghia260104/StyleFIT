aeb879e795c7bd96e8200f9f128ff0c2296874fb
Nantas <vtthinh22@apcs.fitus.edu.vn>
2024-12-28 03:13:18 +0700

    change profile, change password

--- server/account/views.py
+++ server/account/views.py
@@ -1,15 +1,14 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from django.contrib.auth import login
+from rest_framework.permissions import AllowAny
+# from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.contrib.auth import login, authenticate
 from .models import Account
 from .serializers import *
 
 class AccountViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
-    # authentication_classes = [JWTAuthentication]
     permission_classes = [AllowAny]
 
     def get_serializer_class(self):
@@ -17,12 +16,11 @@ class AccountViewSet(viewsets.ModelViewSet):
             return RegistrationSerializer
         elif self.action == 'login':
             return LoginSerializer
-        return None
-
-    # def get_permissions(self):
-    #     if self.action in ['register', 'login']:
-    #         return [AllowAny()]
-    #     return [IsAuthenticated()]
+        elif self.action == 'change_password':
+            return PasswordChangeSerializer
+        elif self.action == 'update_profile_info':
+            return ProfileUpdateSerializer        
+        return AccountSerializer
 
     @action(detail=False, methods=['post'])
     def register(self, request):
@@ -47,14 +45,6 @@ class AccountViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['post'])
     def login(self, request):
         serializer = LoginSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     return Response({
-        #         "message": "Login successful",
-        #         "email": serializer.validated_data['user'].email,
-        #         "role": serializer.validated_data['user'].role,
-        #         "tokens": serializer.validated_data['tokens']
-        #     }, status=status.HTTP_200_OK)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         if serializer.is_valid():
             user = serializer.validated_data
             login(request, user)
@@ -72,6 +62,62 @@ class AccountViewSet(viewsets.ModelViewSet):
                     "profile_photo": user.profile_photo,
                 }
             }, status=status.HTTP_200_OK)
+        return Response({
+            "status": "error",
+            "message": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+    @action(detail=False, methods=['post'])
+    def change_password(self, request):
+        serializer = PasswordChangeSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                user = Account.objects.get(username=serializer.validated_data['username'])
+                user.set_password(serializer.validated_data['newpassword'])
+                user.save()
+                return Response({
+                    "status": "success",
+                    "message": "Password changed successfully"
+                }, status=status.HTTP_200_OK)
+            except Account.DoesNotExist:
+                return Response({
+                    "status": "error",
+                    "message": "User not found"
+                }, status=status.HTTP_404_NOT_FOUND)
+        return Response({
+            "status": "error",
+            "message": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['post'])
+    def update_profile_info(self, request):
+        serializer = ProfileUpdateSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                user = Account.objects.get(username=serializer.validated_data['username'])
+                
+                if 'profile_name' in serializer.validated_data and serializer.validated_data['profile_name'] != '':
+                    user.profile_name = serializer.validated_data['profile_name']
+
+                if 'address' in serializer.validated_data and serializer.validated_data['address'] != '':
+                    user.address = serializer.validated_data['address']
+                
+                user.save()
+                return Response({
+                    "status": "success",
+                    "message": "Profile updated successfully",
+                    "user": {
+                        "profile_name": user.profile_name,
+                        "address": user.address
+                    }
+                }, status=status.HTTP_200_OK)
+            except Account.DoesNotExist:
+                return Response({
+                    "status": "error",
+                    "message": "User not found"
+                }, status=status.HTTP_404_NOT_FOUND)
         return Response({
             "status": "error",
             "message": serializer.errors
